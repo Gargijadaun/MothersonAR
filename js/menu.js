c0a11a1e41aaa628ea5f4ca74588a4b055d35c84
Gargi Jadaun <128454603+Gargijadaun@users.noreply.github.com>
2025-08-02 13:25:19 +0530

    te

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+// const menuIcon = document.querySelector("#menuIcon");
+// const menuContainer = document.querySelector("#menuContainer");
+// const replayBtn = document.querySelector("#replayButton"); // Get Replay button
+
+// const toggleMenu = () => {
+//     menuIcon.classList.toggle("open");
+//     menuContainer.classList.toggle("show");
+
+//     // ✅ Hide the Replay button when menu is opened
+//     replayBtn.style.display = "none";
+// };
+
+// menuIcon.addEventListener("click", toggleMenu);
